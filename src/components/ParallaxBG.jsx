e3bf2fdb6645af9ef8d721b34d0a36e4c21c296f
Rawan-abdelfattah <rwanabdelfattah301@gmail.com>
2025-04-23 20:25:19 +0200

    ParallaxBG

--- src/components/ParallaxBG.jsx
+++ src/components/ParallaxBG.jsx
@@ -3,13 +3,14 @@ import React from "react";
 const ParallaxBG = ({ image, text }) => {
   return (
     <div
-      className="relative bg-fixed bg-center bg-cover h-screen flex items-center justify-center"
+      className="relative bg-center bg-cover h-screen flex items-center justify-center
+                 sm:bg-fixed bg-scroll"
       style={{ backgroundImage: `url(${image})` }}
     >
       {text && (
         <div className="group text-white text-3xl font-bold text-center">
           {text}
-          <div className="mt-4 mx-auto h-1  bg-white rounded w-1/7 transition-all duration-500 group-hover:w-1/5"></div>
+          <div className="mt-4 mx-auto h-1 bg-white rounded w-1/7 transition-all duration-500 group-hover:w-1/5"></div>
         </div>
       )}
     </div>
