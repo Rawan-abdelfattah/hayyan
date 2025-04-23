dfab8033bb776dfb3e6ecf8f1b024a99e8a376dd
Rawan-abdelfattah <rwanabdelfattah301@gmail.com>
2025-04-23 17:05:39 +0200

    edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='bg-black text-center py-10 text-white'>Copyright © 2025 uae sales office    </div>
+    <div className='bg-[#191919] text-center py-10 text-white'>Copyright © 2025 uae sales office    </div>
   )
 }
