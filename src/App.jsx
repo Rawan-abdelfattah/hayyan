e3d484e454143eaaab3bb0adb68d0c99a3337bb5
Rawan-abdelfattah <rwanabdelfattah301@gmail.com>
2025-04-24 14:49:34 +0200

    show data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Hayyan } from "./pages/Hayyan";
 import ThankYouHayyan from "./pages/ThankYouHayyan";
+import { SheetsData } from "./pages/SheetsData";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Hayyan />} />
         <Route path="/thank-you-hayyan" element={<ThankYouHayyan />} />
+        <Route path="/data" element={<SheetsData />} />
       </Routes>
     </Router>
   );
